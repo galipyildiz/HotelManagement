7327fdb0f7a1f98cda082424f4303a0682052cb5
galipyildiz <galipyildiz9606@outlook.com>
2024-05-10 17:24:56 +0300

    feat: 404 page

---
+++ hotelmanagement-ui/src/pages/NotFound/NotFound.js
@@ -0,0 +1,56 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Error404Png from "../../assets/error-404.png";
+
+function NotFound() {
+  return (
+    <Box
+      component="main"
+      sx={{
+        alignItems: "center",
+        display: "flex",
+        justifyContent: "center",
+        minHeight: "100%",
+      }}
+    >
+      <Stack spacing={3} sx={{ alignItems: "center", maxWidth: "md" }}>
+        <Box>
+          <Box
+            component="img"
+            alt="error-404"
+            src={Error404Png}
+            sx={{
+              display: "inline-block",
+              height: "auto",
+              maxWidth: "100%",
+              width: "400px",
+            }}
+          />
+        </Box>
+        <Typography variant="h3" sx={{ textAlign: "center" }}>
+          404: The page you are looking for isn't here
+        </Typography>
+        <Typography
+          color="text.secondary"
+          variant="body1"
+          sx={{ textAlign: "center" }}
+        >
+          You either tried some shady route or you came here by mistake.
+          Whichever it is, try using the navigation
+        </Typography>
+        <Button
+          //   component={RouterLink}
+          //   href={paths.home}
+          //   startIcon={<ArrowLeftIcon fontSize="var(--icon-fontSize-md)" />}
+          variant="contained"
+        >
+          Go back to home
+        </Button>
+      </Stack>
+    </Box>
+  );
+}
+
+export default NotFound;
