65fbc1c6f65306d963428a8a7f064502b02b872e
galipyildiz <galipyildiz9606@outlook.com>
2024-05-12 11:37:46 +0300

    feat: buildings get

---
+++ hotelmanagement-ui/src/pages/Buildings/ApiEndPoints.js
@@ -0,0 +1,5 @@
+export const getAllBuildingsEndPoint = '/Buildings/GetAllBuildings';
+export const getGetBuildingByIdEndPoint = '/Buildings/GetBuildingById'; //query parameter?id=1;
+export const addBuildingEndPoint = '/Buildings/AddBuilding';
+export const deleteBuildingEndPoint = '/Buildings/DeleteBuilding'; //query parameter?id=1
+export const updateBuildingEndPoint = '/Buildings/UpdateBuilding'; //query parameter?id=1;
--- hotelmanagement-ui/src/pages/Buildings/Buildings.js
+++ hotelmanagement-ui/src/pages/Buildings/Buildings.js
@@ -1,7 +1,117 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { api, useInterceptor } from "../../utils/api";
+import { getAllBuildingsEndPoint } from "./ApiEndPoints";
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { Add, Delete, Edit } from "@mui/icons-material";
 
 function Buildings() {
-  return <div>Buildings</div>;
+  useInterceptor();
+  const [buildings, setBuildings] = useState([]);
+
+  useEffect(() => {
+    getBuildingsReq();
+  }, []);
+
+  const getBuildingsReq = async () => {
+    try {
+      const response = await api.get(getAllBuildingsEndPoint);
+      setBuildings(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleAddBuildingClick = async (e) => {
+    e.preventDefault();
+    console.log("add");
+  };
+
+  const handleDeleteBuildingClick = async (e, buildingId) => {
+    e.preventDefault();
+    console.log(buildingId, "delete");
+  };
+
+  const handleEditBuildingClick = async (e, buildingId) => {
+    e.preventDefault();
+    console.log(buildingId, "edit");
+  };
+
+  return (
+    <div>
+      <Typography variant="h5" marginBottom={5}>
+        Buildings
+      </Typography>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          marginBottom: "5px",
+        }}
+      >
+        <Button
+          onClick={(e) => handleAddBuildingClick(e)}
+          variant="outlined"
+          color="success"
+        >
+          <Add />
+          <span>Add Building</span>
+        </Button>
+      </div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell style={{ width: "100px" }} align="right"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {buildings.map((building) => (
+              <TableRow
+                key={building.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>{building.id}</TableCell>
+                <TableCell>{building.name}</TableCell>
+                <TableCell align="right">
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Button
+                      onClick={(e) => handleEditBuildingClick(e, building.id)}
+                    >
+                      <Edit color="secondary" />
+                    </Button>
+                    <Button
+                      onClick={(e) => handleDeleteBuildingClick(e, building.id)}
+                    >
+                      <Delete color="error" />
+                    </Button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
 
 export default Buildings;
